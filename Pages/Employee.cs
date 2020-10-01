6a8db7477c76fa1ee6a838a9a7aa0b1672153b27
Asavani <39687038+Asavani@users.noreply.github.com>
2020-10-01 16:12:31 +0530

    Add project files.

---
+++ Pages/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTask.Pages
+{
+    public class Employee
+    {
+        int id, salary;
+        String name, loc, dep;
+
+        public int Id { get => id; set => id = value; }
+        public int Salary { get => salary; set => salary = value; }
+        public string Name { get => name; set => name = value; }
+        public string Loc { get => loc; set => loc = value; }
+        public string Dep { get => dep; set => dep = value; }
+    }
+}
---
+++ Pages/handler.cshtml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyTask.Pages;
+
+namespace MyTask
+{
+    public class handlerModel : PageModel
+    {
+        List<String> employee_name = new List<String>();
+        List<Employee> employee_data = new List<Employee>();
+        public ActionResult OnGet()
+        {
+            List<Employee> employee_data = new List<Employee>();
+            Employee e = new Employee();
+            e.Id = 1;
+            e.Name = "Anil Asavani";
+            e.Salary = 26000;
+            e.Loc = "Gandhinagar";
+            e.Dep = "Developing";
+            employee_data.Add(e);
+            Employee e1 = new Employee();
+            e1.Id = 2;
+            e1.Name = "Sunil Asavani";
+            e1.Salary = 35000;
+            e1.Loc = "Ahmedabad";
+            e1.Dep = "Testing";
+            employee_data.Add(e1);
+            /*employee_name.Add("Anil");
+            employee_name.Add("Sunil");
+            employee_name.Add("Jayesh");
+            employee_name.Add("Manan");*/
+            return Content(JsonSerializer.Serialize(employee_data));
+        }
+        public JsonResult OnGetEmployee_find(int salary)
+        {
+            Employee e = new Employee();
+            e.Id = 1;
+            e.Name = "Anil Asavani";
+            e.Salary = 26000;
+            e.Loc = "Gandhinagar";
+            e.Dep = "Developing";
+            employee_data.Add(e);
+            Employee e1 = new Employee();
+            e1.Id = 2;
+            e1.Name = "Sunil Asavani";
+            e1.Salary = 35000;
+            e1.Loc = "Ahmedabad";
+            e1.Dep = "Testing";
+            employee_data.Add(e1);
+            Employee es=employee_data.Find(x => x.Salary == salary);
+            return new JsonResult(es);
+        }
+     
+        public void onPost()
+        {
+
+        }
+    }
+}
